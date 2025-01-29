2c82590b37e8b8465c5cf43f756dd6f3b17e1a7e
Zach Harris <code@zharr.is>
2025-01-29 01:52:46 -0500

    rm -rf

--- emacs/literate-config/lisp/org-pandoc-import/preprocessors/rmarkdown.el
+++
@@ -1,8 +0,0 @@
-;;; org-pandoc-import/preprocessors/rmarkdown.el --- Pre-process Rmarkdown files before importing -*- lexical-binding: t; -*-
-;;; SPDX-License-Identifier: GPL-3.0-or-later
-
-(defun org-pandoc-import-rmarkdown-preprocessor (in-file)
-  (let ((processed-file (make-temp-file "opif" nil ".Rmd")))
-    (call-process "sed" nil (list :file processed-file) nil "s/^```{/```\\n{/" in-file)
-    (message processed-file)
-  processed-file))
